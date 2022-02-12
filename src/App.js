074a923610b2c920c5c7525506166f1f4266e60f
Shashi <shashikant.py@gmail.com>
2022-02-12 23:14:15 +0530

    paragrapg

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import data from './data'
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // console.log(typeof count);                   //typeof means number will be in string
+    let amouunt = parseInt(count);
+
+    // setting amount 
+
+    if (amouunt <= 0){
+      amouunt = 1
+    }
+    if (amouunt > 8){
+      amouunt = 8
+    }
+    // for seting the value user is requesting  usue slice method is return a new copy of array and start to end
+    setText(data.slice(0,amouunt));     // last is not count 
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="section-center">
+      <h3> tried of boring ipsum</h3>
+      <form className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="amount">
+          paragraphs:
+        </label>
+        <input type='number' name='amount' id='amount' value={count}     //type in number
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button type="submit" className="btn">generate</button>
+      </form>
+      <article className="lorem-text">
+        {text.map((item, index) =>{
+          return <p key={index}>{item}</p>
+        })}
+      </article>
+    </section>
   );
 }
 
